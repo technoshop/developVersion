168cf814d55ecbd4907861a70506eb8f61713856
khadonis <kaan.demirkol@outlook.com>
2018-01-11 23:41:19 +0300

    harici pluginler yüklenildi

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
 import { Container } from '../../theme/grid';
 import { Title } from '../Projects/Projects.style';
-import {Spot} from './About.style';
+import { Spot } from './About.style';
+import ScrollMagic from 'scrollmagic';
+import {addIndicator} from './debug.addIndicators';
 
 class About extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            magic: '',
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('load', this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('load', this.handleResize);
+    }
+
+    handleResize() {
+        // init
+        var controller = new ScrollMagic.Controller({
+            globalSceneOptions: {
+                triggerHook: 'onLeave'
+            }
+        });
+
+        // get all slides
+        var slides = document.querySelectorAll("section.panel");
+
+        // create scene for every slide
+        for (var i = 0; i < slides.length; i++) {
+            new ScrollMagic.Scene({
+                triggerElement: slides[i]
+            })
+                .setPin(slides[i])
+                .addTo(controller);
+        }
+    }
+
     render() {
         return (
-            <Container>
+            <Container magic={this.handleResize()}>
                 <Title>About Us</Title>
                 <Spot>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, provident nobis. Quam voluptates tenetur, corrupti expedita eos hic cupiditate odit.</Spot>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quia, error exercitationem dolores ipsum explicabo harum cum eius maxime nobis culpa temporibus molestias fugit, mollitia accusantium nisi quae ea? Eveniet ad molestiae repudiandae blanditiis dolorum magni reiciendis ullam sunt aperiam quas sequi nesciunt, labore enim illum quis sed! Rem, recusandae!</p>
+                <section className="panel blue">
+                    <b>ONE</b>
+                </section>
+                <section className="panel turqoise">
+                    <b>TWO</b>
+                </section>
+                <section className="panel green">
+                    <b>THREE</b>
+                </section>
+                <section className="panel bordeaux">
+                    <b>FOUR</b>
+                </section>
             </Container>
         );
     }
--- src/Containers/About/About.style.js
+++ src/Containers/About/About.style.js
@@ -1,5 +1,44 @@
-import styled from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
 
 export const Spot = styled.h2`
     margin: 90px 0;    
 `;
+injectGlobal`
+.panel{
+    height: 100vh;
+    width: 100%;
+}
+.panel.green{
+    margin-bottom: 400px;
+}
+.blue {
+	background-color: #3883d8;
+}
+.turqoise {
+	background-color: #38ced7;
+}
+.brown {
+	background-color: #a66f28;
+}
+.bordeaux {
+	background-color: #953543;
+}
+.skin {
+	background-color: #ED9F4C;
+}
+.black {
+	background-color: #000000;
+}
+.white {
+	background-color: #FFFFFF;
+}
+.red {
+	background-color: #cf3535;
+}
+.green {
+	background-color: #22d659;
+}
+.orange {
+	background-color: #ea6300;
+}
+`
