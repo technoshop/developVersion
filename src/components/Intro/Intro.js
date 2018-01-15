27cbc2d4d8f8a9886d66b6444a9c35061eb4f1f9
khadonis <kaan.demirkol@outlook.com>
2018-01-15 22:54:38 +0300

    slider eklendi

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react';
 import { ContainerFluid } from 'Src/theme/grid';
+import Slide from './Slide';
 import { } from './Intro.style';
 
-const styles = {
-    transition: 'all ease .3s',
-}
 
+export default class Slider extends Component {
 
-class Intro extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             listOfPosts: this.props.itemsPost,
-			currentPostId: this.props.itemsPost[0].id
+            currentPostId: this.props.itemsPost[0].id
         }
-
-      
+        this.currentPost = this.currentPost.bind(this);
     }
 
+    currentPost(e) {
+        let getNext = e.target.getAttribute('data-post');
+        let element = document.getElementById("post");
+        setTimeout(() => {
+            this.setState({
+                currentPostId: getNext
+            });
+            element.style.opacity = "0";
+        }, 300);
 
-    
-    
-    
-   
-
+    }
 
     render() {
+
+        var current = this.state.currentPostId;
+        var items = this.props.itemsPost.filter(function (l) {
+            return l.id == current;
+        });
+
         return (
             <div>
-                <ul id="nav">
-                    <li >bir</li>
-                    <li >iki</li>
-                    <li >üç</li>
-                </ul>
-                <ul id="slider">
-                    <li>slide 1</li>
-                    <li>slide 2</li>
-                    <li>slide 3</li>
-                </ul>
+                <Slide {...items[0]} currentPost={this.currentPost} />
             </div>
         );
     }
 }
 
-export default Intro;
+
+
+
