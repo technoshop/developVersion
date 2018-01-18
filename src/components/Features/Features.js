89e6a35f999cff96cfc78411a904505352fb853c
khadonis <kaan.demirkol@outlook.com>
2018-01-19 00:18:36 +0300

    features'a stiller veriliyor...

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Btn } from './Features.style';
+import { ContainerFluid, Container } from 'Src/theme/grid';
 
 
 
@@ -7,33 +8,33 @@ var features = [
     {
         "link": "Digital Lightfield",
         "description": `Our lightfield photonics generate digital light at different depths and blend seamlessly with natural light to produce lifelike digital objects that coexist in the real world. This advanced technology allows our brain to naturally process digital objects the same way we do real-world objects, making it comfortable to use for long periods of time.`,
-        "id":"0"
-        
+        "id": "0"
+
     },
     {
         "link": "Visual Perception",
         "description": `The robust sensor suite on Magic Leap One detects surfaces, planes and objects, allowing for digital reconstruction of your physical surroundings. The result is a system that sees what you see, allowing lightfield objects to not only exist in the physical world but actually interact with it. Whether it’s virtual displays sitting alongside the computer monitor on your desk or a virtual panda that climbs across your living room couch, visual perception with machine learning unlocks the power of spatial computing.`,
-        "id":"1"
-        
+        "id": "1"
+
     },
     {
         "link": "Persistent Objects",
         "description": `Our visual perception and room-mapping technology builds a digital replica of your physical environment – detecting and storing the precise location of walls, surfaces and other physical objects. Lightfield objects stay where you put them, just as they would in real life. Place a virtual TV on the wall over your fireplace and when you return later, the TV will be right where you left it.`,
-        "id":"2"
+        "id": "2"
     },
 ]
 
 
+
 class ListItem extends Component {
-
-
+    
     render() {
         let color = 'red';
         return (
             this.props.features.map((feature, i) => {
-                
+
                 return <li key={i} >
-                    <Btn value={i} className={feature.id === this.props.index && 'active'} onClick={this.props.onClick.bind(this)} index={this.props.index} >
+                    <Btn id="btn" value={i} className={feature.id === this.props.index && 'active'} onClick={this.props.onClick.bind(this)} index={this.props.index} >
                         {feature.link}
                     </Btn>
                 </li>
@@ -42,7 +43,6 @@ class ListItem extends Component {
     }
 
 }
-
 
 var indexx
 class Features extends Component {
@@ -54,9 +54,10 @@ class Features extends Component {
             link: features[0].link,
             description: features[0].description,
             style: null,
-            index: null,
+            index: 0,
         }
     }
+
     onClick(e) {
         e.preventDefault();
         indexx = e.target.value;
@@ -74,23 +75,28 @@ class Features extends Component {
                 })
             }, 300)
         }
-
-
     }
+
+    
+
     render() {
         let description = this.state.description;
         return (
-            <div>
-                <h2>Platform Features</h2>
-                <ul>
-                    <ListItem index={this.state.index} onClick={this.onClick} features={this.state.features} />
-                </ul>
-                <h3 className="feature-subhead" style={{ opacity: this.state.style }}>{this.state.link}</h3>
-                <div style={{ opacity: this.state.style }} className="feature-carousel">
-                    {this.state.description}
+            <Container>
+                <div className="features">
+                    <div className="feaures-list">
+                        <h2 className="features-list__head" >Platform Features</h2>
+                        <ul>
+                            <ListItem index={this.state.index} onClick={this.onClick} features={this.state.features} />
+                        </ul>
+                    </div>
+                    <h3 className="feature-subhead" style={{ opacity: this.state.style }}>{this.state.link}</h3>
+                    <div style={{ opacity: this.state.style }} className="feature-carousel">
+                        {this.state.description}
+                    </div>
                 </div>
 
-            </div>
+            </Container>
         );
     }
 }
--- src/components/Features/Features.style.js
+++ src/components/Features/Features.style.js
@@ -1,5 +1,6 @@
 import styled, { injectGlobal } from "styled-components";
 import * as vars from "../../theme/variables";
+import bgImg from '../../assets/05b370a36c2febc613ef9d1060e0af74.webp';
 
 export const Btn = styled.button`
     background: none;
@@ -21,6 +22,23 @@ injectGlobal`
       opacity: 1;
     }
   }
+  .features {
+      width: 100%;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-image: url(${bgImg});
+      background-size: 60%;
+      height: 100vh;
+  }
+  .feaures-list{
+      width: 25%;
+      float: left;
+  }
+  .features-list__head{
+      font-size: 3em;
+      color: ${vars.color_2018};
+  }
+
     .feature-carousel, .feature-subhead {
         transition: all ease-in-out .5s;
 
